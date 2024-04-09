c321016df7dded22d06586fe03261f538407e4e5
Bryan See <bryanscis@gmail.com>
2024-04-08 21:57:40 -0700

    Add explicit datatypes for Player dataframe and create function to return dataframe of n recent games

--- scrape.py
+++ scrape.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
+from datetime import datetime
 
 class Player:
     def __init__(self):
@@ -15,8 +17,33 @@ class Player:
             cur = []
             for column in columns:
                 stat, value = column['data-stat'], column.get_text()
-                if stat in ['fg', 'fga', 'fg_pct', 'fg3', 'fg3a', 'fg3_pct', 'ft', 'fta', 'ft_pct', 'orb', 'drb', 'trb', 'ast', 'stl', 'blk', 'tov', 'pf', 'pts'] and value:  
-                    value = float(value)
+                if stat in ['fg', 'fga', 'fg_pct', 'fg3', 'fg3a', 'fg3_pct', 'ft', 'fta', 'ft_pct', 'orb', 'drb', 'trb', 'ast', 'stl', 'blk', 'tov', 'pf', 'pts', 'game_score', 'plus_minus']:  
+                    value = float(value) if value else np.nan
+                elif stat == 'mp' and value:
+                    value = '00:' + value
                 cur.append(value)
             rows.append(cur)
-        return pd.DataFrame(rows, columns=['game_season', 'date_game', 'age', 'team_id','game_location', 'opp_id','game_result', 'gs', 'mp', 'fg', 'fga', 'fg_pct', 'fg3', 'fg3a', 'fg3_pct', 'ft', 'fta', 'ft_pct', 'orb', 'drb', 'trb', 'ast', 'stl', 'blk', 'tov', 'pf', 'pts', 'game_score', 'plus_minus' ])
+        df = pd.DataFrame(rows, columns=['game_season', 'date_game', 'age', 'team_id','game_location', 'opp_id','game_result', 'gs', 'mp', 'fg', 'fga', 'fg_pct', 'fg3', 'fg3a', 'fg3_pct', 'ft', 'fta', 'ft_pct', 'orb', 'drb', 'trb', 'ast', 'stl', 'blk', 'tov', 'pf', 'pts', 'game_score', 'plus_minus' ])
+        df = df.astype(dtype = {'game_season': "object", 'date_game': "str", 'age': "str", 'team_id': "str", 'game_location': "str", 'opp_id': "str", 
+                               'game_result': "str", 'gs': "str", 'mp': "str", 'fg': "Int64", 'fga': "Int64", 'fg_pct': "float64", 'fg3': "Int64", 'fg3a': "Int64", 
+                               'fg3_pct': "float64", 'ft': "Int64", 'fta': "Int64", 'ft_pct': "float64", 'orb': "Int64", 'drb': "Int64", 'trb': "Int64", 'ast': "Int64", 'stl': "Int64",
+                               'blk': "Int64", 'tov': "Int64", 'pf': "Int64", 'pts': "Int64", 'game_score': "float64", 'plus_minus':"float64" })
+        return df
+    
+    def recent_games(self, n= 5, total=True):
+        '''
+        Prints recent n games and creates a subset of the dataframe with additional row of averages or totals.
+
+        Params:
+        - n (int): most recent n games
+        - total (boolean): True for total or False for averages
+        '''
+        recent_df = self.df.iloc[-n:].copy()
+        mp = pd.to_timedelta(recent_df["mp"], errors='coerce')
+        if total:
+            recent_df.loc["total", ~recent_df.columns.isin(['fg_pct', 'fg3_pct', 'ft_pct'])] = recent_df.sum(numeric_only=True)
+            recent_df.loc["total", "mp"] = mp.sum()
+        else:
+            recent_df.loc["avg"] = round(recent_df.mean(numeric_only=True), ndigits=2)
+            recent_df.loc["avg", "mp"] = mp.mean().round('1s')
+        return recent_df
