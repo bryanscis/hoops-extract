bd0f40e337a2870f47f8f5e7d7bfe785ab67c4fe
Bryan See <bryanscis@gmail.com>
2024-04-18 18:37:37 -0700

    Add per game and per 36 minutes dataframe functions

--- roster.py
+++ roster.py
@@ -5,6 +5,7 @@ import numpy as np
 class Roster:
 
     def __init__(self):
+        self.cols = ['player', 'age', 'g', 'gs', 'mp', 'fg', 'fga', 'fg%', '3p', '3pa', '3p%', '2p', '2pa', '2p%', 'efg%', 'ft', 'fta', 'ft%', 'orb', 'drb', 'trb', 'ast', 'stl', 'blk', 'tov', 'pf', 'pts']
         self.df = self.scrape_roster()
 
     def scrape_roster(self):
@@ -20,7 +21,24 @@ class Roster:
                     value = np.nan
                 cur.append(value)
             rows.append(cur)
-        df = pd.DataFrame(rows, columns=['player', 'age', 'g', 'gs', 'mp', 'fg', 'fga', 'fg%', '3p', '3pa', '3p%', '2p', '2pa', '2p%', 'efg%', 'ft', 'fta', 'ft%', 'orb', 'drb', 'trb', 'ast', 'stl', 'blk', 'tov', 'pf', 'pts'])
-        df = df.astype(dtype= {'player': 'str', 'age': 'Int64', 'g': 'Int64', 'gs': 'Int64', 'mp': 'Int64', 'fg': 'Int64', 'fga': 'Int64', 'fg%': 'float64', '3p': 'Int64', '3pa': 'Int64', '3p%':'float', 
-                               '2p': 'Int64', '2pa': 'Int64', '2p%': 'float', 'efg%': 'float', 'ft':'Int64', 'fta': 'Int64', 'ft%': 'float', 'orb': 'Int64', 'drb': 'Int64', 'trb':'Int64',
-                                'ast': 'Int64', 'stl': 'Int64', 'blk': 'Int64', 'tov': 'Int64', 'pf': 'Int64', 'pts': 'Int64'})
+        df = pd.DataFrame(rows, columns=self.cols)
+        df = df.astype(dtype= {'player': 'str', 'age': 'int', 'g': 'int', 'gs': 'int', 'mp': 'float', 'fg': 'float', 'fga': 'float', 'fg%': 'float', '3p': 'float', '3pa': 'float', '3p%':'float', 
+                               '2p': 'float', '2pa': 'float', '2p%': 'float', 'efg%': 'float', 'ft':'float', 'fta': 'float', 'ft%': 'float', 'orb': 'float', 'drb': 'float', 'trb':'float',
+                                'ast': 'float', 'stl': 'float', 'blk': 'float', 'tov': 'float', 'pf': 'float', 'pts': 'float'})
+        return df
+        
+    def per_game(self):
+        '''
+        Returns dataframe of per game statistics of NBA roster.
+        '''
+        pg_df = self.df.copy() 
+        pg_df.iloc[:, 4:] = round(pg_df.iloc[:, 4:].div(pg_df.g, axis=0), ndigits=2)
+        return pg_df
+
+    def per_36(self):
+        '''
+        Returns dataframe of per 36 minute statistic of NBA roster. Calculated by dividing stat by minutes played and then multiplying by 36.
+        '''
+        p36_df = self.df.copy()
+        p36_df.iloc[:, 4:] = round(p36_df.iloc[:, 4:].div(p36_df.mp, axis=0).mul(36, axis=0), ndigits=2)
+        return p36_df
