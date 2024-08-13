3486dd1d61904ad9e3d677050e4e795b57e6d32f
Bryan See <bryanscis@gmail.com>
2024-08-12 18:04:16 -0700

    Add functionality to extract game statistics while extracting box scores

--- dags/extract/scripts/extract_box_scores.py
+++ dags/extract/scripts/extract_box_scores.py
@@ -14,13 +14,18 @@ def extract_box_scores():
     '''
     year = get_current_season()
     schedule_file = f'/opt/airflow/data/{year}/schedules/nba_{year}_schedule.csv'
+    stats_file = f'/opt/airflow/data/{year}/nba_{year}_statistics.csv'
     
     if not Path(schedule_file).is_file():
         raise FileNotFoundError(f"Schedule file {schedule_file} not found")
+    if not Path(stats_file).is_file():
+        with open(stats_file, 'w') as write:
+            writer = csv.writer(write, delimiter='\t')
     logging.info(f'Schedule file {schedule_file} found.')
 
-    with open(schedule_file, 'r') as f:
+    with open(schedule_file, 'r') as f, open(stats_file, 'a', newline="") as write:
         reader = csv.reader(f, delimiter='\t')
+        writer = csv.writer(write, delimiter='\t')
         for row in reader:
             game_date = datetime.strptime(row[0], '%a, %b %d, %Y')
             if game_date.date() < datetime.now().date():
@@ -32,6 +37,9 @@ def extract_box_scores():
                         current_game = Game(home_team, away_team, row[0], row[1], year)
                         total_box_df =  pd.concat([current_game.home_box(),current_game.away_box()], ignore_index=True)
                         total_box_df.to_csv(f'{game_file}', sep='\t', index=False, header=False)
-                        logging.info(f'Game file {home_team} vs {away_team} on {game_date.date()} has been created at "{game_file}".')
+                        game_stats = current_game.get_statistics().values()
+                        logging.info(f'Game file {home_team} vs {away_team} on {game_date.date()} has been created at "{game_file}". Writing to game_stats file with {game_stats}')
+                        writer.writerow(game_stats)
+                        logging.info(f'Finished writing {home_team} vs {away_team} on {game_date.date()} to statistics file.')
                 except Exception as e:
                     logging.info(f'Error occured while getting game on {game_date.date()} for {home_team} vs {away_team}: {e}')
