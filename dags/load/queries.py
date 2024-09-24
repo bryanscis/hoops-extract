1e9b6948f61ab6300d54e58eee63cbd603bfde38
Bryan See <bryanscis@gmail.com>
2024-09-23 19:14:16 -0700

    Add functionality to load box score information into database

--- dags/load/queries.py
+++ dags/load/queries.py
@@ -1,6 +1,8 @@
 select_season_query = ("SELECT season_id FROM season WHERE season_year = %s")
 select_player_id = ("SELECT player_id FROM player WHERE first_name = %s AND last_name = %s;")
 select_team_id = ("SELECT team_id FROM team WHERE abbreviation = %s")
+select_game_id = ("SELECT game_id FROM game WHERE home_team_id = %s AND away_team_id = %s AND game_date = %s AND season_year = %s")
+select_player_team = ("SELECT p.player_id FROM player p JOIN player_team_season pts ON p.player_id = pts.player_id WHERE p.first_name = %s AND p.last_name = %s AND (p.suffix = %s OR p.suffix IS NULL)AND pts.team_id = ANY(%s)")
 
 insert_player_query = ("""
     INSERT INTO player (first_name, last_name, suffix, position, height, weight, pre_draft_team, draft_pick, nationality)
@@ -19,4 +21,9 @@ insert_player_team_season_query = ("""
     DO UPDATE SET age = EXCLUDED.age,
                 current_team = EXCLUDED.current_team
     WHERE player_team_season.current_team = TRUE OR EXCLUDED.current_team = TRUE;
+""")
+
+insert_player_stats = ("""
+    INSERT INTO player_stats (game_id, player_id, minutes_played, fg_made, fg_attempted, threes_made, threes_attempted, ft_made, ft_attempted, orb, drb, rebounds, assists, steals, blocks, turnovers, fouls, points, plus_minus, inactive)
+                                           VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
 """)
---
+++ dags/load/scripts/load_box_scores.py
@@ -0,0 +1,44 @@
+import csv
+import logging
+from airflow.hooks.postgres_hook import PostgresHook
+from dags.load.queries import select_game_id, select_team_id, select_player_team, insert_player_stats
+from pathlib import Path
+from scripts.misc import split_name
+from utils import get_current_season
+
+def load_box_scores(**kwargs):
+    '''
+    Loads box scores into PostgreSQL database after extraction and transformation.
+    '''
+    file_paths = kwargs['dag_run'].conf.get('new_games', [])
+    hook = PostgresHook(postgres_conn_id='nba_connection')
+    conn = hook.get_conn()
+    cursor = conn.cursor()
+    try:
+        for file_name in file_paths:
+            file = Path(file_name).stem
+            date, home_team, away_team = str(file[:8]), str(file[8:11]), str(file[11:])
+            cursor.execute(select_team_id, (home_team,))
+            home_team_id = cursor.fetchone()[0]
+            cursor.execute(select_team_id, (away_team,))
+            away_team_id = cursor.fetchone()[0]
+            cursor.execute(select_game_id, (home_team_id, away_team_id, date, get_current_season()))
+            game_id = cursor.fetchone()[0]
+            with open(file_name, 'r') as rf:
+                reader = csv.reader(rf, delimiter='\t')
+                for row in reader:
+                    first_name, last_name, suffix = split_name(row[0])
+                    cursor.execute(select_player_team, (first_name, last_name, suffix, [home_team_id, away_team_id]))
+                    current_player_id = cursor.fetchone()[0]
+                    cursor.execute(insert_player_stats, (game_id, current_player_id, row[1], int(float(row[2])), int(float(row[3])), int(float(row[5])), 
+                                                         int(float(row[6])), int(float(row[8])), int(float(row[9])), int(float(row[11])), int(float(row[12])), 
+                                                         int(float(row[13])), int(float(row[14])), int(float(row[15])), int(float(row[16])), int(float(row[17])), 
+                                                         int(float(row[18])), int(float(row[19])), int(float(row[21])), True if row[1] == '00:00:00' else False ))
+        conn.commit()
+    except Exception as e:
+        logging.info(f"Error {e} on file: {file_name}.")
+        conn.rollback()
+        raise e
+    finally:
+        cursor.close()
+        conn.close()
